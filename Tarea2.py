3fd65c19ef421df4801e8690b4fb849738cf2a95
RojasCJP <jprojaschinchilla@gmail.com>
2020-08-20 11:15:05 -0600

    modificaciones menores

--- Tarea2.py
+++ Tarea2.py
@@ -1,34 +1,38 @@
 import json
-from xml.dom import minidom;
+from xml.dom import minidom
 
 # aqui empieza el proceso para archivo json
 
 # se abre el archivo json y se lee
 with open('JSONfile.json') as f:
-    data = json.load(f)
+    dataJson = json.load(f)
 
 # se muestra el archivo json en consola y de una manera legible
 print("json file down")
-print()
-for person in data['people']:
-    print(person['nombre'], person['apellido'], person['edad'], person['estado'], sep = ", ")
+print(type(dataJson['people']))
+registroJsonNo = 1
+for person in dataJson['people']:
+    print("registro " + str(registroJsonNo))
+    print(person['nombre'], person['apellido'], person['edad'], person['estado'], sep=", ")
+    registroJsonNo += 1
 
 # aqui empieza el proceso para archivo xml
 # se parsea el archivo xml a python
 dataXml = minidom.parse("XMLfile.xml");
 
-# se agregan los datos del archivo xml a un arreglo con cada campo
-clave = [dataXml.getElementsByTagName("clave")[0], dataXml.getElementsByTagName("clave")[1], dataXml.getElementsByTagName("clave")[2], dataXml.getElementsByTagName("clave")[3]]
-nombre = [dataXml.getElementsByTagName("nombre")[0], dataXml.getElementsByTagName("nombre")[1], dataXml.getElementsByTagName("nombre")[2], dataXml.getElementsByTagName("nombre")[3]]
-apellido = [dataXml.getElementsByTagName("apellido")[0], dataXml.getElementsByTagName("apellido")[1], dataXml.getElementsByTagName("apellido")[2], dataXml.getElementsByTagName("apellido")[3]]
-carnet = [dataXml.getElementsByTagName("carnet")[0], dataXml.getElementsByTagName("carnet")[1], dataXml.getElementsByTagName("carnet")[2], dataXml.getElementsByTagName("carnet")[3]]
-
 # se muestran en consola los campos del archivo xml
 print()
 print()
 print("xml file down")
-print()
-print(clave[0].firstChild.data, nombre[0].firstChild.data, apellido[0].firstChild.data, carnet[0].firstChild.data, sep = ", ")
-print(clave[1].firstChild.data, nombre[1].firstChild.data, apellido[1].firstChild.data, carnet[1].firstChild.data, sep = ", ")
-print(clave[2].firstChild.data, nombre[2].firstChild.data, apellido[2].firstChild.data, carnet[2].firstChild.data, sep = ", ")
-print(clave[3].firstChild.data, nombre[3].firstChild.data, apellido[3].firstChild.data, carnet[3].firstChild.data, sep = ", ")
+print(type(dataXml))
+estudianteNo = 0
+#se agregan a cada variable un campo de el archivo xml y luego se imprime para que tenga sierto formato
+while estudianteNo <= 3:
+    clave = dataXml.getElementsByTagName("clave")[estudianteNo]
+    nombre = dataXml.getElementsByTagName("nombre")[estudianteNo]
+    apellido = dataXml.getElementsByTagName("apellido")[estudianteNo]
+    carnet = dataXml.getElementsByTagName("carnet")[estudianteNo]
+    print("registro " + str(estudianteNo + 1))
+    print(clave.firstChild.data, nombre.firstChild.data, apellido.firstChild.data, carnet.firstChild.data, sep=", ")
+    estudianteNo += 1
+
